15058ac517480274bcd3f1b1cb85959823c5767d
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-05-17 20:23:11 +0100

    Endpoint to view single user request

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -7,8 +7,21 @@ class RequestController {
   static getAllRequests(req, res) {
     return res.status(200).json({
       allRequests: requests,
-      status: 'Success',
       message: 'All requests by user',
+    });
+  }
+
+  // Get single request from data
+  static getSingleRequest(req, res) {
+    const foundRequest = requests.find(request => request.id === parseInt(req.params.id, 10));
+    if (foundRequest) {
+      return res.status(200).json({
+        request: foundRequest,
+        message: 'Single request by user',
+      });
+    }
+    return res.status(404).json({
+      message: 'Request not found!',
     });
   }
 }
