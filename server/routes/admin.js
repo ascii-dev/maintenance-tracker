aad7080f5932bde5d94cb0737327dc7a481d0805
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-05-29 03:58:24 +0100

    feat(require): create endpoint for admin to disapprove request
    - create the endpoint route
    
    - create the endpoint controller
    
    - secure the route with JWT and only admin can access
    
    [finishes #157798085]

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,6 +5,7 @@ import AdminController from '../controllers/adminController';
 
 adminRoutes.get('/requests', VerifyToken, VerifyAdmin, AdminController.getAllRequests);
 adminRoutes.get('/requests/:id', VerifyToken, VerifyAdmin, AdminController.getSingleRequest);
-adminRoutes.put('/requests/:id', VerifyToken, VerifyAdmin, AdminController.approveRequest);
+adminRoutes.put('/requests/:id/approve', VerifyToken, VerifyAdmin, AdminController.approveRequest);
+adminRoutes.put('/requests/:id/disapprove', VerifyToken, VerifyAdmin, AdminController.disapproveRequest);
 
 export default adminRoutes;
