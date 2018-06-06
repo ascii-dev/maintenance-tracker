9b567b8189acca8892976a6dbec268c63bd7c9b7
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-06-06 12:18:05 +0100

    feat(request): user can create new request
    - import create request ui
    
    - process create request data
    
    [finishes #158143244]

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -7,5 +7,6 @@ frontendRoutes.get('/', (req, res) => { res.sendFile('index.html', { root }); })
 frontendRoutes.get('/signup', (req, res) => { res.sendFile('signup.html', { root }); });
 frontendRoutes.get('/login', (req, res) => { res.sendFile('login.html', { root }); });
 frontendRoutes.get('/dashboard', (req, res) => { res.sendFile('dashboard.html', { root }); });
+frontendRoutes.get('/create', (req, res) => { res.sendFile('create.html', { root }); });
 
 export default frontendRoutes;
