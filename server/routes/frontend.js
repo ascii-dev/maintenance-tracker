3dd7d2eb0f38b8142200129bec281abed5c8219e
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-06-06 06:33:44 +0100

    feat(auth): user can login
    - import login ui
    
    - create login route and process login using the api
    
    [finishes #158138093]

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -5,6 +5,7 @@ const root = 'frontend/views';
 
 frontendRoutes.get('/', (req, res) => { res.sendFile('index.html', { root }); });
 frontendRoutes.get('/signup', (req, res) => { res.sendFile('signup.html', { root }); });
+frontendRoutes.get('/login', (req, res) => { res.sendFile('login.html', { root }); });
 frontendRoutes.get('/dashboard', (req, res) => { res.sendFile('dashboard.html', { root }); });
 
 export default frontendRoutes;
