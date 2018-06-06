e3023be600e1d176807f8a0d7f6289777706fe62
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-06-06 06:17:45 +0100

    feat(auth): user can create account
    - import signup ui
    
    - create route and process signup using the api
    
    [finishes #158103677]

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -4,5 +4,7 @@ const frontendRoutes = Router();
 const root = 'frontend/views';
 
 frontendRoutes.get('/', (req, res) => { res.sendFile('index.html', { root }); });
+frontendRoutes.get('/signup', (req, res) => { res.sendFile('signup.html', { root }); });
+frontendRoutes.get('/dashboard', (req, res) => { res.sendFile('dashboard.html', { root }); });
 
 export default frontendRoutes;
