2831ac2d59332d76845c4d316b9ae8d24745dec5
Samuel Afolaranmi <rafolaranmi@gmail.com>
2018-06-07 03:40:35 +0100

    feat(request): user can modify request
    - create script to modify request through api endpoint
    
    - create form to display request details for modification
    
    [finishes #158148321]

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -9,5 +9,7 @@ frontendRoutes.get('/login', (req, res) => { res.sendFile('login.html', { root }
 frontendRoutes.get('/dashboard', (req, res) => { res.sendFile('dashboard.html', { root }); });
 frontendRoutes.get('/create', (req, res) => { res.sendFile('create.html', { root }); });
 frontendRoutes.get('/requests/:id', (req, res) => { res.sendFile('single.html', { root }); });
+frontendRoutes.get('/requests/edit/:id', (req, res) => { res.sendFile('modify.html', { root }); });
+
 
 export default frontendRoutes;
